feb5634e4d4d8d06ed9431ee2d34dd9365fae44b
Yasin Bhojani <yasinbhojani333@gmail.com>
2022-12-11 19:38:26 +0530

    reload logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,13 @@ import React from 'react'
 import styles from "./Header.module.css"
 
 const Header = () => {
+  const onReload = () => {
+    window.location.reload();
+  }
+
   return (
     <header className={styles.header}>
-      <h1>Movie Search</h1>
+      <h1 onClick={onReload}>Movie Search</h1>
     </header>
   )
 }
